d25eb1f2f2902e7712902d35254af142b13669dc
Marcin Rzeźnicki <marcin.rzeznicki@gmail.com>
2020-04-26 02:22:32 +0200

    Implement table-driven test generation
    Instances for 2 and 3-ary tables
    Tests

--- src/Test/Hspec/Tables.hs
+++ src/Test/Hspec/Tables.hs
@@ -1,7 +1,48 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+
 -- |
 -- Copyright: (c) 2020 Marcin Rzeźnicki
 -- SPDX-License-Identifier: MIT
 -- Maintainer: Marcin Rzeźnicki <marcin.rzeznicki@gmail.com>
 --
 -- Table-driven (by example) HSpec tests
-module Test.Hspec.Tables where
+module Test.Hspec.Tables
+  ( byExample,
+  )
+where
+
+import Test.Hspec.Core.Spec
+
+class Table r where
+  type Header r
+  type Forall r p
+  apply :: Forall r p -> r -> p
+
+  showHeader :: Header r -> String
+  default showHeader :: Show (Header r) => Header r -> String
+  showHeader = show
+
+instance Table (a, b) where
+  type Header (a, b) = (String, String)
+  type Forall (a, b) p = a -> b -> p
+  apply = uncurry
+
+instance Table (a, b, c) where
+  type Header (a, b, c) = (String, String, String)
+  type Forall (a, b, c) p = a -> b -> c -> p
+  apply f (a, b, c) = f a b c
+
+byExample ::
+  forall a r.
+  (Table r, Example a, Show r) =>
+  Header r ->
+  [r] ->
+  Forall r a ->
+  SpecWith (Arg a)
+byExample header table test =
+  describe (showHeader @r header) $ mapM_ (\row -> specify @a (show row) $ apply test row) table
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,54 @@
-module Main (main) where
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE RecordWildCards #-}
 
+import Control.Monad (join)
+import Data.Foldable
+import Data.Functor ((<&>))
+import Test.Hspec
+import Test.Hspec.Core.Runner (ColorMode (..), Config (..), Summary (..), defaultConfig, runSpec)
+import Test.Hspec.Core.Spec (Item (..), runSpecM)
+import Test.Hspec.Tables
 
 main :: IO ()
-main = putStrLn ("Test suite is not implemented" :: String)
+main = hspec spec
+
+spec :: Spec
+spec = forM_ [spec2Bool, spec2BoolF, spec2Expectation, spec3] specMetaSpec
+
+data MetaSpec b = forall a. Show a => MetaSpec {label :: String, def :: [a], thisSpec :: SpecWith b, expectedFailures :: Int}
+
+withFailures :: MetaSpec a -> Int -> MetaSpec a
+withFailures meta failures = meta {expectedFailures = failures}
+
+describeMeta :: (Show a) => String -> [a] -> ([a] -> SpecWith b) -> MetaSpec b
+describeMeta label def generateSpec = MetaSpec label def generatedSpec 0
+  where
+    generatedSpec = generateSpec def
+
+spec2Bool :: MetaSpec ()
+spec2Bool = describeMeta "spec2Bool" [(0, 0), (1, 2), (2, 4), (3, 6), (4, 8)] $ \examples -> byExample ("A", "expected") examples (\a expected -> expected == a * 2)
+
+spec2BoolF :: MetaSpec ()
+spec2BoolF = describeMeta "spec2BoolF" [(0, 0), (1, 2), (2, 4), (3, 6), (4, 8)] (\examples -> byExample ("A", "expected") examples (==)) `withFailures` 4
+
+spec2Expectation :: MetaSpec ()
+spec2Expectation = describeMeta "spec2Expectation" [(0, 0), (1, 2), (2, 4), (3, 6), (4, 8)] $ \examples -> byExample ("A", "expected") examples (shouldBe . (* 2))
+
+spec3 :: MetaSpec ()
+spec3 = describeMeta "spec3" [(0, 0, 0), (1, 1, 2), (2, 2, 4), (3, 3, 6), (4, 4, 8), (5, 5, 10)] $ \examples -> byExample ("A", "B", "expected") examples (\a b expected -> a + b == expected)
+
+_config :: Config
+_config = defaultConfig {configIgnoreConfigFile = True, configColorMode = ColorNever}
+
+specMetaSpec :: MetaSpec () -> Spec
+specMetaSpec MetaSpec {..} = describe label $ do
+  specify "Analysis" $ expectCorrectSpec def thisSpec
+  let expectedSummary = Summary (length def) expectedFailures
+  specify "Execution" $ runSpec thisSpec _config `shouldReturn` expectedSummary
+
+expectCorrectSpec :: Show a => [a] -> SpecWith b -> Expectation
+expectCorrectSpec def generatedSpec = runSpecM generatedSpec <&> getAllItems >>= (shouldMatchList labels . itemRequirements)
+  where
+    itemRequirements = map itemRequirement
+    getAllItems = join . map toList
+    labels = map show def
